2294d298b05d336c0b2c8f97ef4a5f3a5b253035
greenwood12 <100176593+greenwood12@users.noreply.github.com>
2022-03-31 18:20:20 +0900

    3-31
    3-31

--- 001.HTML학습/07.CGV_PJ/003.site_v2/js/main.js
+++ 001.HTML학습/07.CGV_PJ/003.site_v2/js/main.js
@@ -14,3 +14,67 @@ function chgMV(mvid) { //mvid - 영화아이디값 전달변수
     //src="https://www.youtube.com/embed/-5Dc8EMVYBo"
 }
 ///////////////////////// chgMV /////////////////////////////
+
+
+//////// 로딩구역 ///////////////////////////////////////////////////////////////
+window.addEventListener("load", ()=>{
+    console.log("로딩완료!");
+    // 포스터 a요소 클릭시 영화 변경하기
+    // 대상 : .mlist a
+
+    const mcode = {
+        "듄":"-5Dc8EMVYBo",
+        "유체이탈자":"_fuaoomS7zs",
+        "이터널스":"'BdkSkI61aGo",
+        "연애 빠진 로맨스":"dWEQjU3GCE0",
+        "프렌치 디스패치":"Y1_Ujpsn1Jc",
+        "스파이더맨:노웨이홈":"",
+    };
+
+
+    let mlink = document.querySelectorAll(".mlist a");
+    console.log("a링크개수 :", mlink.length);
+    // 클릭이벤트 속성 셋팅하기
+    for(let x of mlink) {
+        x.onclick = () => {            
+            // 각 a요소를 구분해주는 것
+            // -> 자식요소 중 img의 alt 속성에 영화 제목있음!
+            let mtit = x.querySelector("img").getAttribute("alt");
+            console.log("영화제목", mtit);
+
+            //2.영화코드객체에서 값을 읽어와서 chgMV함수 호출
+            chgMV(mcode[mtit]);
+
+            //3. a요소 #으로 인한 상단튐 방지하기
+            return false;
+        };
+        // click 함수 ///////////////////////////////////////////////////////////
+    }
+    /// for of///////////////////////////////////////////////////////////////////
+
+
+    // 포스터 메뉴 li 클릭시 li에 클래스 on넣기
+    // 대상 : .glist ul>li
+    let mlist = document.querySelectorAll(".mlist ul>li");
+    console.log("리스트개수", mlist.length);
+
+    const resetFn = ()=>{        
+        for(let x of mlist) {            
+            x.classList.remove("on");
+        }    
+    };
+
+    // 대상만큼 click이벤트 설정하기
+    // for of 사용!/////////////////////////////////////////////////////////////
+    for(let x of mlist) {
+        //1. 클래스 초기화함수 호출        
+        //2. 해당 li요소 클래스 on 넣기
+        x.onclick = ()=>{
+            resetFn();            
+            x.classList.add("on");
+        };
+        /// click ///////////////////////////////////////////////////////////////
+    }
+    // for of///////////////////////////////////////////////////////////////////
+});
+//////// 로딩구역 ///////////////////////////////////////////////////////////////
--- 과제/eldenring_pj/js/common.js
+++ 과제/eldenring_pj/js/common.js
@@ -5,48 +5,101 @@ function getParameterByName(name) {
     results = regex.exec(location.search); 
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " ")); 
 }
+/****************************************************************************************************************/ 
+// 두번째 페이지 업데이트 정보
+const open = (val) => {    
+    let popupText = new Array("업데이트 파일 배포 공지"+
+                                "2022.03.17"+
+                                "게임 플레이의 안정성 개선 및 밸런스 조정을 위한 업데이트 파일을 배포하였습니다." +
+                                "최신 업데이트 파일을 적용하신 후 플레이해 주시기 바랍니다." +
+                                "대상 플랫폼" +
+                                "PlayStation®4/PlayStation®5/Xbox One/Xbox Series X|S/STEAM®", //자르기
+                                "업데이트 파일 배포 안내 (PS5™/PC)" +
+                                "2022.03.02" +
+                                "PlayStation®5 버전 및 PC 버전의 게임 플레이 안정성을 개선하는 업데이트 파일을 배포하였습니다." +    
+                                "최신 버전 업데이트 파일을 설치해 주시기 바랍니다." +
+                                "최신 업데이트에 포함되는 주요 항목" +
+                                "PlayStation®5 버전" +
+                                "・게임 종료 시 이외에도 세이브 데이터가 저장되도록 하기 위한 변경", //자르기
+                                "PS4™/PS5™ 업그레이드 기능에 대한 안내" +
+                                "2022.02.28" +
+                                "PlayStation®4/PlayStation®5 ‘ELDEN RING(엘든 링)’에 대해," +
+                                "PlayStation®4 버전에서 PlayStation®5 버전으로의 업그레이드가 불가능한 현상을 확인 하고 있습니다."+
+                                "불편을 겪고 계신 고객 여러분께 진심으로 사과 드립니다. 현재 원인 확인 중에 있으며, 조금만 기다려 주시면 감사 드리겠습니다."+
+                                "확인이 되는 대로 재차 공지 드리도록 하겠습니다.");
+    document.querySelector(".txt_info").innerHTML = popupText[val];
+    document.querySelector(".modal").classList.remove("hidden");
+}
 
-// const open = (val) => {
-//     alert(val);
-//     return;
-//     let popupText = new Array("11", "22", "33");
-//     document.querySelector(".txt_info").innerHTML = popupText[val];
-//     document.querySelector(".modal").classList.remove("hidden");
-// }
-
-//https://goddino.tistory.com/74
-
+// 팝업창 닫기
 const close = () => {
     document.querySelector(".modal").classList.add("hidden");
 }
+/****************************************************************************************************************/ 
 
+/****************************************************************************************************************/ 
+//세번째 페이지 전역변수
+let textNm = 0;
+let txt_Third = new Array("다채로운 상황이 기다리고 있는 오픈 필드와<br>"+
+                    "복잡하면서도 입체적으로 만들어진 거대한 던전이<br>"+
+                    "끊임없이 이어지는 광대한 세계.<br>"+
+                    "세계를 탐험하다 미지의 발견에서 오는 기쁨과 큰 달성감을 안겨 줄 위협적인 존재들이 <br>"+
+                    "플레이어를 기다리고 있습니다.", //자르기
+                    "기동력이 뛰어나 험준한 벼랑을 타고 내려갈 수 있는 영마와<br>"+
+                    "공략의 폭을 넓히는 점프 액션을 채용.<br>"+
+                    "광대한 필드와 복잡한 던전을 자유롭고 쾌적하게 탐색할 수 있습니다.", //자르기                    
+                    "플레이어 캐릭터는 외견을 커스터마이즈할 수 있으며<br>"+
+                    "장착할 무기와 방어구, 마법까지 마음껏 조합할 수 있습니다.<br>"+
+                    "근력을 키워 강건한 전사가 되거나 마술을 연마하는 등<br>"+
+                    "자신의 플레이 스타일에 맞춰 캐릭터를 성장시킬 수 있습니다."
+                    );
+/****************************************************************************************************************/                   
 
-
-// ulr 파라미터 값 추출
+// 로딩후 함수
 const loadFn = function () {    
-    let pageNum = getParameterByName("page");    
-    //console.log(pageNum);
-    page_Select(parseInt(pageNum));    
+    // ulr 파라미터 값 추출
+    let pageNum = parseInt(getParameterByName("page"));    
+    //pageNum 값이 없을 경우
+    if(pageNum ===null || pageNum ==="" || isNaN(pageNum)) pageNum = 1;
+    page_Select(pageNum);
 
-    document.querySelector("#openBtn1").onclick = () => {
-        let popupText = "1111";
-        document.querySelector(".txt_info").innerHTML = popupText;
-        document.querySelector(".modal").classList.remove("hidden");
+    //2번째 페이지 일 경우 셋팅
+    if(pageNum === 2) {
+        document.querySelector(".openBtn1").onclick = () => open(0);    
+        document.querySelector(".openBtn2").onclick = () => open(1);    
+        document.querySelector(".openBtn3").onclick = () => open(2);
+        document.querySelector(".modal").onclick = () => close();
+    } else if (pageNum === 3) {
+        let setimg = document.getElementById("setImg");
+        let txtdetail = document.querySelector(".txtdetail");
+        
+        setimg.src = "../image/img_concept_01.jpg";
+        txtdetail.innerHTML = txt_Third[0];
 
-    };
 
-    document.querySelector("#openBtn2").onclick = () => {
-        let popupText = "2222";
-        document.querySelector(".txt_info").innerHTML = popupText;
-        document.querySelector(".modal").classList.remove("hidden");
+        let buttenArry = document.querySelectorAll(".arrowTo");
+        buttenArry[0].onclick =() => {            
+            if(textNm === 2){
+                textNm = 0;
+            } else {
+                textNm ++;
+            }            
+            setimg.src = `../image/img_concept_0${textNm+1}.jpg`;
+            txtdetail.innerHTML = txt_Third[textNm];
+        };
 
-    };
-
-    document.querySelector("#openBtn3").onclick = () => {
-        let popupText = "333";
-        document.querySelector(".txt_info").innerHTML = popupText;
-        document.querySelector(".modal").classList.remove("hidden");
-    };
+        buttenArry[1].onclick =() => {
+            console.log(textNm);
+            if(textNm === 0){
+                textNm = 2;
+            } else {
+                textNm --;
+            }
+            console.log(textNm);
+            setimg.src = `../image/img_concept_0${textNm+1}.jpg`;
+            txtdetail.innerHTML = txt_Third[textNm];
+        };
+    }
 };
 
 // 버튼 변경
@@ -57,6 +110,7 @@ function page_Select(num) {
     let mkArray3 = new Array("btn9", "btn10", "btn11", "btn12");
     let mkArray4 = new Array("btn13", "btn14", "btn15", "btn16");
     let contentArray = new Array(".container1", ".container2", ".container3", ".container4");
+    let backImgArray = new Array("thumb_info.png", "thumb_wp_pc_01.jpg", "thumb_wp_pc_06.jpg", "thumb_wp_pc_07.jpg");
 
     //버튼 정보
     let btn_data;
@@ -76,7 +130,14 @@ function page_Select(num) {
         default : save_color = "";        
     }
 
-    // 선택한 정보 보이기
+    // 선택한 정보 보이기    
+    let main_text = document.querySelectorAll(".main_text");    
+    for(let x of main_text) {
+        let count = 0;
+        x.style.background =`url(../image/${backImgArray[num-1]}) no-repeat center/cover`;
+        count++;
+    }
+
     document.getElementById(btn_data[num-1]).style.marginBottom ="50px";
     document.getElementById(btn_data[num-1]).style.backgroundColor = save_color;
     document.querySelector(contentArray[num-1]).style.display = "block";
@@ -96,14 +157,6 @@ function page_Select(num) {
         }
     });
 }
-
-
-// document.querySelector(".openBtn1").addEventListener("click", open(0));
-// document.querySelector(".openBtn2").addEventListener("click", open(1));
-// document.querySelector(".openBtn3").addEventListener("click", open(2));
-// document.querySelector(".closeBtn").addEventListener("click", close);
-// document.querySelector(".bg").addEventListener("click", close);
-
 
 // 로딩 후 실행 함수
 //window.addEventListener("DOMContentLoaded",loadFn);
